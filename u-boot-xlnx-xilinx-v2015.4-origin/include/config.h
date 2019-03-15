eea8a4a8d318bbf27f4c94810f45053b39212913
Alexey Korevko <mollex85@gmail.com>
2019-03-15 14:52:34 +0300

    add *.c *.h *.S *.dtb
    Signed-off-by: Alexey Korevko <mollex85@gmail.com>

---
+++ u-boot-xlnx-xilinx-v2015.4-origin/include/config.h
@@ -0,0 +1,7 @@
+/* Automatically generated - do not edit */
+#define CONFIG_BOARDDIR board/xilinx/zynq
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/zynq_zed.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>

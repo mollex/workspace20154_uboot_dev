da8d7af25583c607dab8dae46f2e9647385a0864
Alexey Korevko <mollex85@gmail.com>
2019-03-25 14:14:15 +0300

    add u-boot-xlnx-xilinx-v2015.4-debug prj / compile, debug success
    Signed-off-by: Alexey Korevko <mollex85@gmail.com>

---
+++ u-boot-xlnx-xilinx-v2015.4-debug/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,3 @@
+#define U_BOOT_DATE "Feb 19 2019"
+#define U_BOOT_TIME "18:06:18"
+#define U_BOOT_TZ "+0400"
---
+++ u-boot-xlnx-xilinx-v2015.4-debug/include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2015.07"
+#define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
+#define CC_VERSION_STRING "arm-xilinx-linux-gnueabi-gcc (Sourcery CodeBench Lite 2015.05-17) 4.9.2"
+#define LD_VERSION_STRING "GNU ld (Sourcery CodeBench Lite 2015.05-17) 2.24.51.20140217"
